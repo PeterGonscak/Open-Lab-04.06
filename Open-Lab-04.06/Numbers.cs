6c908179ff815d8380037dc270038a5577723089
Peter Gonščák <peter.gonscak190@gmail.com>
2021-11-20 11:19:25 +0100

    bez listu a kniznic xD. done

--- Open-Lab-04.06/Numbers.cs
+++ Open-Lab-04.06/Numbers.cs
@@ -5,8 +5,20 @@ namespace Open_Lab_04._06
     public class Numbers
     {
         public int[] NoOdds(int[] numbers)
-        {
-            throw new NotImplementedException();
+        { 
+            int a = 0;
+            for (int i = 0; i < numbers.Length; i++)
+                if (numbers[i] % 2 == 0)
+                    a++;
+            int[] filter = new int[a];
+            a = 0;
+            for (int i = 0; i < numbers.Length; i++)
+                if (numbers[i] % 2 == 0)
+                {
+                    filter[a] = numbers[i];
+                    a++;
+                }
+            return filter;     
         }
     }
 }
